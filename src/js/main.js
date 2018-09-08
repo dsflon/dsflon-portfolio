d29db755b0df102a187b6dd8cf7a4183cace9f96
Daiki Saito <dsflon@gmail.com>
2018-09-08 12:45:33 +0900

    firebase initに関して修正

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,3 @@
-import firebase from 'firebase/app';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -8,23 +6,11 @@ import { createStore } from 'redux'
 import reducer from './reducers'
 
 import Root from './_root';
+import FireInit from './fireinit';
 import mgnUa from 'mgn-ua';
 
 window.ua = new mgnUa();
 window.html = document.getElementsByTagName('html')[0];
-
-/*
-** Firebase Initialize
-*/
-const config = {
-    apiKey: "AIzaSyBjnA-8mNLtluLH69mnFP7oFcJrHF8C4z4",
-    authDomain: "dsflon-portfolio.firebaseapp.com",
-    databaseURL: "https://dsflon-portfolio.firebaseio.com",
-    projectId: "dsflon-portfolio",
-    storageBucket: "dsflon-portfolio.appspot.com",
-    messagingSenderId: "216748225524"
-};
-firebase.initializeApp(config);
 
 /*
 ** Create Store
